141b98b3b82682b39ed162b21ea2c49ef7d0641d
Nikhil Pandey <nikhilpandey@Nikhils-MacBook-Air.local>
2025-06-03 19:35:03 +0530

    UPDATE: video scrapper

--- app/celery_app.py
+++ app/celery_app.py
@@ -30,7 +30,7 @@ celery.conf.update(
 celery.conf.beat_schedule = {
     "update-ad-insights": {
         "task": "app.insights_async.update_ad_insights",
-        "schedule": crontab(minute="*"),
+        "schedule": crontab(hour=0, minute=0),
     },
     # "update-ad-insights": {
     #     "task": "app.meta_ads_library.meta_library_ads_sync",
